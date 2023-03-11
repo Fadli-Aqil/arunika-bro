2d17b1e83bc580d0878c27e85d3c987c04cff738
Fadli-Aqil <fadli.aqil12@gmail.com>
2023-03-11 11:19:58 +0700

    add new calculating payment

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-const info = (x) => {
-    let y = x * 5;
-    let z = y * x;
-    alert(z);
+class Payment {
+    #discount
+    #ppn
+
+    constructor (discount) {
+        this.#discount = discount ? discount : 0
+        this.#ppn = 0.1
+    }
+
+    checkItem(item) {
+        return `Nama Item: ${item}`
+    }
+
+    hitungPayment(cash) {
+        const price = cash * this.#discount
+        const total = cash - price
+        const pajak = total * this.#ppn
+        const total_price = total + pajak
+        return `Total Pembayaran: ${total_price}`
+    }
+
+    summary(cash, item){
+        alert(`Summary: 
+            ${this.checkItem(item)}
+            ${this.hitungPayment(cash)}
+            `)
+    }
 }
+
+const inject = (cash, item, discount) => {
+    const payment = new Payment(discount)
+    payment.summary(cash, item)
+};
